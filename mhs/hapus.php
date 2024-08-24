5d21d97897a86e37efdb705a87053605a7760077
farhanzayid1982 <farhan.zayid@gmail.com>
2024-08-24 20:14:24 +0700

    modified:   mhs/hapus.php

--- mhs/hapus.php
+++ mhs/hapus.php
@@ -1,3 +1,24 @@
-<?php
-    echo "HAPUS";
+<?php    
+    echo "'proses':'HAPUS MAHASISWA',";
+    $parameter = "";
+    if(isset($od["nim"]) and $od["nim"]!="") {
+        $nim = $od["nim"];
+        
+        $parameter = "'" . $nim . "'";
+    }
+    $sql = "call hapusMHS(" . $parameter . ");";
+    
+    include_once("conn.php");
+    
+    
+    //Metode 1
+    if($conn->query($sql) === TRUE) {
+        //Jika penghapusan berhasil menampilkan notifikasi penghapusan berhasil
+        echo "'status':'SUKSES','pesan':'Data terhapus.'";
+    } else {
+        //Penghapusan gagal
+        echo '{"status":"ERROR", "pesan":"Error: ' . $sql . '<br>' . $conn->error . '"}';
+    }
+
+    $conn->close();
 ?>
