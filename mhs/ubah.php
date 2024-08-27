eff76018f696e9f5ca5b29df5e7c4a4546f9c711
farhanzayid1982 <farhan.zayid@gmail.com>
2024-08-27 20:05:53 +0700

    modified:   dosen/ubah.php 	modified:   mhs/ubah.php

--- mhs/ubah.php
+++ mhs/ubah.php
@@ -1,3 +1,52 @@
 <?php
-    echo "UBAH";
+    echo "'proses':'UBAH MHS',";
+    $parameter = "";
+    if(isset($od["nim"]) and $od["nim"]!="") {
+        $oldnim = $od["oldnim"];
+        $nim = $od["nim"];
+        $nama = $od["nama"];
+        $tanggal_lahir = $od["tanggal_lahir"];
+        $tempat_lahir = $od["tempat_lahir"];
+        $jenis_kelamin =  $od["jenis_kelamin"];
+        $masuk = $od["masuk"];
+        $keluar = $od["keluar"];
+        $parameter = "'" . $oldnim . "', '". $nim . "', '" . $nama . "', '" . $tempat_lahir . "', '" . $tanggal_lahir . "', '" . $jenis_kelamin . "', '" . $masuk . "', '" . $keluar . "'";
+    }
+    $sql = "call updateMHS(" . $parameter . ");";
+    
+    include_once("conn.php");
+    
+    /*
+    //Metode 1
+    if($conn->query($sql) === TRUE) {
+        //Jika penyimpanan berhasil menampilkan notifikasi penyimpanan berhasil
+        echo "'status':'SUKSES','pesan':'Data tersimpan.'";
+    } else {
+        //Penyimpanan gagal
+        echo '{"status":"ERROR", "pesan":"Error: ' . $sql . '<br>' . $conn->error . '"}';
+    }
+    */
+    
+    //Metode 2
+    $result = $conn->query($sql);
+    if($result->num_rows >0) {
+        echo "'data':[";
+        $counter = 0;
+        // Output data dari setiap baris
+        while($row = $result->fetch_assoc()) {
+            //$counter++;
+            //if($counter>1) {echo ",";};
+            $status = $row["rstatus"];
+            $pesen = $row["rpesan"];
+        }
+        echo "],";
+        echo "'status':'$status',";
+        echo "'pesan':'" . $result->num_rows . "'";
+    } else {
+        echo "'status':'ERROR',";
+        echo "'pesan':'Tidak Ada Data'";
+    }
+
+    //Tutup koneksi
+    $conn->close();
 ?>
