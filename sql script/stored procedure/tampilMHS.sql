6b88341b6890d81a339e1b68f8a5fc26950c33ca
farhanzayid1982 <117135454+farhanzayid1982@users.noreply.github.com>
2024-09-05 10:22:31 +0700

    modified:   sql script/stored procedure/tampilMHS.sql

--- sql script/stored procedure/tampilMHS.sql
+++ sql script/stored procedure/tampilMHS.sql
@@ -1,2 +1,6 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `tampilMHS`(IN `filter` VARCHAR(255))
-select * from v_mhs
+if LENGTH(`filter`)=0 then
+   select * from v_mhs;
+ELSE
+	SELECT * from v_mhs WHERE nama like CONCAT('%',`filter`,'%') or nim like CONCAT('%',`filter`,'%');
+END IF
