bf9660cad1cdfd93c7bed3011c49a135a4845d0c
farhanzayid1982 <117135454+farhanzayid1982@users.noreply.github.com>
2025-01-30 18:48:46 +0700

    modified:   index_method.php

--- index_method.php
+++ index_method.php
@@ -4,16 +4,23 @@
     { 
         case 'GET':
             echo "Request Methode GET";
+            //include_once('dosen\tampil.php');
             break;
         case 'POST':
             echo "Request Methode POST";
+            //include_once('dosen\sisip.php');
             break;
         case 'PUT':
             echo "Request Methode PUT";
+            //include_once('dosen\ubah.php');
             break;
         case 'DELETE':
             echo "Request Methode DELETE";
+            //include_once('dosen\hapus.php');
             break;
+        default:
+            echo "'status':'ERROR',";
+            echo "'pesan':'Perintah tidak sesuai'";
     }
 
     echo "}"; //akhir pembentukan data JSON
