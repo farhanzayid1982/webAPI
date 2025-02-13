f9e638beb2d7d0a30c5ae9b6176d04a53e22ec35
farhanzayid1982 <117135454+farhanzayid1982@users.noreply.github.com>
2025-02-13 21:11:33 +0700

    modified:   index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
             break;
             
         case 'POST':
-            echo "Request Methode POST";
+            //echo "Request Methode POST";
             $json_params = file_get_contents("php://input");
             $data = json_decode(file_get_contents('php://input'), true);
             $od = $data["data"]; //Object Data
@@ -68,19 +68,19 @@
                     include_once('dosen\sisip.php');
                     break;
                 case 'sisipMHS':
-                    include_once('dosen\sisip.php');
+                    include_once('mhs\sisip.php');
                     break;
             }
             break;
         case 'PUT':
-            echo "Request Methode PUT";
+            //echo "Request Methode PUT";
             $json_params = file_get_contents("php://input");
             $data = json_decode(file_get_contents('php://input'), true);
             $od = $data["data"]; //Object Data
             include_once('dosen\ubah.php');
             break;
         case 'DELETE':
-            echo "Request Methode DELETE";
+            //echo "Request Methode DELETE";
             if(isset($_GET['pid'])) {
                 $pid=$_GET['pid'];
             } else {
